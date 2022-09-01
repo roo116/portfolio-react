fbf345f693497455ab02b415ff7d16220e7bbfaf
Chip Hill <hillkarl03@gmail.com>
2022-09-01 02:24:28 -0400

    rendered basic About Me section with React

---
+++ src/ components/About/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2>About Me</h2>
+      <p>
+        {" "}
+        Seeking to build web applications and grow as a developer. Skills
+        include - JavaScript, CSS, JQuery, responsive web design, Node.js,
+        Express, mySql. I work under a "no-quit clause", and I solve problems. I
+        seek to increase my understanding and pursue mastery of web development
+        while providing services to local companies.
+      </p>
+    </div>
+  );
+};
+
+export default About;
