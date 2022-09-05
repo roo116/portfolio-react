c11d23d9645ec76b21f06c70bd3173a1aa24059b
Chip Hill <hillkarl03@gmail.com>
2022-09-05 12:23:02 -0400

    created contact form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import Nav from "./components/Nav";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
+import ContactForm from "./components/Contact";
 
 function App() {
   const [categories] = useState([{ name: "My Work" }, { name: "Resume" }]);
@@ -17,6 +18,7 @@ function App() {
       <main>
         <About></About>
         <Portfolio></Portfolio>
+        <ContactForm></ContactForm>
       </main>
     </div>
   );
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,13 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ContactForm() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+
+  function handleChange(e) {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(formState);
+  }
+
   return (
     <section>
       <h1>Contact me</h1>
-      <form id="contact">
-        {/* name input */}
-        {/* email input */}
-        {/* message text area */}
+      <form id="contact" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            defaultValue={name}
+            onChange={handleChange}
+            name="name"
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="text"
+            defaultValue={email}
+            onChange={handleChange}
+            name="email"
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            defaultValue={message}
+            onChange={handleChange}
+            rows="5"
+          />
+        </div>
+        <button type="submit">Submit</button>
       </form>
     </section>
   );
