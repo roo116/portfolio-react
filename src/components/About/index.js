bb053bacdeeff51b2860a722d1c160013fc17a9b
Chip Hill <hillkarl03@gmail.com>
2022-09-05 11:50:18 -0400

    project cards are now displaying

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,24 +3,26 @@ import coverImage from "../../assets/img/lync_photo_bw.jpg";
 
 function About() {
   return (
-    <section className="flex-row space-around">
+    <section className="my-5">
+      <h1 id="about">About Me</h1>
       <img
         src={coverImage}
         className="my-2"
         style={{ width: "100px" }}
         alt="cover"
       />
-      <h2>About Me</h2>
-      <p>
-        {" "}
-        Seeking to build web applications and grow as a developer. Skills
-        include - JavaScript, CSS, JQuery, responsive web design, Node.js,
-        Express, mySql. I work under a "no-quit clause", and I solve problems. I
-        seek to increase my understanding and pursue mastery of web development
-        while providing services to local companies.
-      </p>
+      <div className="my-2">
+        <p>
+          {/* {" "} */}
+          Seeking to build web applications and grow as a developer. Skills
+          include - JavaScript, CSS, JQuery, responsive web design, Node.js,
+          Express, mySql. I work under a "no-quit clause", and I solve problems.
+          I seek to increase my understanding and pursue mastery of web
+          development while providing services to local companies.
+        </p>
+      </div>
     </section>
   );
-};
+}
 
 export default About;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 
+
 function Portfolio() {
-  const project = [
+  const projects = [
     {
       title: "jokr",
       description:
@@ -9,20 +10,76 @@ function Portfolio() {
       github: "https://github.com/roo116/jokr",
       link: "https://damp-bastion-89209.herokuapp.com/",
     },
+    {
+      title: "note-taker",
+      description:
+        "Note application. Utilizes express.js and published to Heroku.",
+      github: "https://github.com/roo116/note-taker",
+      link: "https://upper-mountie-24559.herokuapp.com/",
+    },
+    {
+      title: "Futurama Character Search",
+      description:
+        "Team collaboration utilizing materialize css, and API calls to create a character-search app. Displays the voice-actors behind the characters of Futurama, an animated TV series.",
+      github: "https://github.com/roo116/character-search",
+      link: "https://roo116.github.io/character-search/",
+    },
+    {
+      title: "Weather Dashboard",
+      description:
+        "Utilized BootStrap and Weather One api to create a weather dashboard with realtime weather updates, at a click of a button.",
+      github: "https://github.com/roo116/weather-dashboard",
+      link: "https://roo116.github.io/weather-dashboard/",
+    },
+    {
+      title: "Work-Day Scheduler",
+      description:
+        "Utilized JQuery, localStorage and Third-party utilities to create a scheduling application.",
+      github: "https://github.com/roo116/work-day-scheduler",
+      link: "https://roo116.github.io/work-day-scheduler/",
+    },
+    {
+      title: "Code-Quiz",
+      description:
+        " Sample on-line quiz.  Example of dynamic html creation using JavaScript.",
+      github: "https://github.com/roo116/code-quiz",
+      link: "https://roo116.github.io/code-quiz/",
+    },
   ];
-
   return (
-    <section>
-      <div>
-        <div className="card-body">
+    <section id="portfolio">
+      <h1>My Work</h1>
+      {projects.map((project, i) => (
+        <div className="card" key={project.title}>
+          <div>
+            <img
+              src={require(`../../assets/img/project/${i}.jpg`).default}
+              alt={project.title}
+            />
+          </div>
           <h5>{project.title}</h5>
           <p>{project.description}</p>
-          <a href={project.link}>Link</a>
-          <a href={project.gitub}>GitHub</a>
+          <a href={project.github} target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+          <a href={project.link} target="_blank" rel="noreferrer">
+            Application
+          </a>
         </div>
-      </div>
+      ))}
     </section>
   );
 }
 
 export default Portfolio;
+
+
+//   <h1>title</h1>
+    //   <p>description</p>
+    //   <a href="" alt="">
+    //     GitHub
+    //   </a>
+    //   <a href="" alt="">
+    //     Application
+    //   </a>
+    // </section>
